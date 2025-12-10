a0adfa9a4dc0079c25f9646629da6c93f6ed0baa
murilomunari <munarimurilo441@gmail.com>
2025-12-10 09:09:35 -0300

    first commit

---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+    return(
+        <header>
+            <nav>
+                <div>
+                    <h1>FutApp</h1>
+                </div>
+            </nav>
+        </header>
+    )
+}
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+    return(
+        <div>
+            <h1>Hello esta funcionando</h1>
+        </div>
+    )
+}

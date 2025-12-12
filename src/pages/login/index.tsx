a79e3963f5b3ae64dfae94955b008760465c0f0e
murilomunari <munarimurilo441@gmail.com>
2025-12-11 23:55:01 -0300

    terminar amanha estou com sono

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -10,7 +10,7 @@ export function Login() {
 
     function handleSubmit(event: React.FormEvent) {
         event.preventDefault();
-        
+
         if (email === '' || password === '') {
             alert('Por favor, preencha todos os campos.');
             return;
@@ -23,28 +23,28 @@ export function Login() {
 
             <h1 className="text-white text-4xl font-bold mb-6">
                 Fut
-                <span className="bg-gradient-to-r from-blue-800 to-blue-400 bg-clip-text text-transparent">
+                <span className="bg-gradient-to-r from-blue-800 to-blue-400 bg-clip-text text-transparent drop-shadow-lg">
                     Feed
                 </span>
             </h1>
 
             <form onSubmit={handleSubmit} className="flex flex-col gap-4 w-80">
-                <Input 
-                    placeholder="Digite seu email..." 
-                    type="email" 
-                    value={email} 
+                <Input
+                    placeholder="Digite seu email..."
+                    type="email"
+                    value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
 
-                <Input 
-                    placeholder="Digite sua senha..." 
-                    type="password" 
-                    value={password} 
+                <Input
+                    placeholder="Digite sua senha..."
+                    type="password"
+                    value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 />
 
-                <button 
-                    type="submit" 
+                <button
+                    type="submit"
                     className="bg-blue-600 text-white p-2 rounded shadow hover:bg-blue-700 transition"
                 >
                     Entrar
--- src/pages/teams/index.tsx
+++ src/pages/teams/index.tsx
@@ -1,6 +1,6 @@
 export function SelectTeam() {
     return(
-        <div>
+        <div className="flex justify-center text-center max-w-64 h-full border-2">
             <h1>TESTE time</h1>
         </div>
     )

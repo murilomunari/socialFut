5fa578f71b9a2410330819b38fdec5cfc3ed2881
murilomunari <munarimurilo441@gmail.com>
2025-12-15 17:22:19 -0300

    Refatorar o layout do componente SelectTeam e atualizar o texto para maior clareza.

--- src/pages/teams/index.tsx
+++ src/pages/teams/index.tsx
@@ -1,7 +1,15 @@
 export function SelectTeam() {
-    return(
-        <div className="flex justify-center text-center max-w-64 h-full border-2">
-            <h1>TESTE time</h1>
+    return (
+        <div className="flex min-h-screen">
+            <div className="w-45 bg-white text-center p-4">
+                <h2 className="text-lg - font-bold mb-6 text-center">
+                    Times
+                </h2>
+
+                <p className="text-sm text-center opacity-70">
+                    Selecione um campeonato para ver os times disponíveis.
+                </p>
+            </div>
         </div>
     )
 }
